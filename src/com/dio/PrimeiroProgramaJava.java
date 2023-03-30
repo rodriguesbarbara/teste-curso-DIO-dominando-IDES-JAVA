99c98cf7afa7606b81551ab4437d2c59fc469c4f
Barbara <bahrodrigues620@gmail.com>
2023-03-30 19:44:04 -0300

    "first commit"

---
+++ src/com/dio/PrimeiroProgramaJava.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroProgramaJava {
+
+  public static void main(String[] args) {
+	 
+	  Gato gato = new Gato();	  
+	  
+	  System.out.println(gato)
+	  
+	  ;	 /* int a = 3;
+	  int b = 2;
+	System.out.println("hello world!"  + (a+b));*/
+    
+  }
+}                         
